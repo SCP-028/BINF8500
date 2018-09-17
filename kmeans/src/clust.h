8f3a7d241fe824b9a79e679d1be6a997b609842d
SCP-028 <zhou.zy.yi@gmail.com>
2018-09-17 12:39:30 -0400

    Fix comments.

--- kmeans/src/clust.h
+++ kmeans/src/clust.h
@@ -3,7 +3,7 @@
 #include <string>
 #include <vector>
 #include <sstream> // std::istringstrem
-#include <fstream> // std::ifstream, ofstream
+#include <fstream> // std::ifstream
 #include <cmath>   // pow, sqrt, log, isnan
 #include <limits>  // std::numeric_limits<float>::quiet_NaN();
 
