61c11028f2fcd8cb7bd514f5bbb418b04539655a
SCP-028 <zhou.zy.yi@gmail.com>
2018-09-18 22:15:00 -0400

    Parsing fasta data and constructing score matrix.

--- Needleman_Wunsch/src/main.cpp
+++ Needleman_Wunsch/src/main.cpp
@@ -3,12 +3,53 @@
  * Author: Yi Zhou
 */
 #include "nw.h"
-// Read file in and parse sequence into vector
 
-// Construct score matrix (reverse the sequence)
+using namespace std;
 
-// Calculate score
+const unsigned PRINT_WIDTH = 80;
 
-// Trace back and find the alignment
+int main(int argc, char **argv)
+{
+    if (argc != 6)
+    {
+        printf("[Error] %s takes 5 arguments, but %d were given.\n\n"
+               "Usage: %s <seq1> <seq2> <match_score> <mismatch_score> <gap_penalty>\n\n"
+               "This program implements the Needleman-Wunsch algorithm and\n"
+               "align the two given sequences.\n"
+               "Matches and mismatches have uniform scores, whereas gaps have a linear penalty.\n",
+               argv[0], argc - 1, argv[0]);
+        return 1;
+    }
+    // Read file in and parse sequence into a single string
+    const float MATCH = stof(argv[3]), MISMATCH = stof(argv[4]), GAP = stof(argv[5]);
+    string seq1, seq2;
+    seq1 = nw::read_fasta(argv[1]);
+    seq2 = nw::read_fasta(argv[2]);
+    nw::prepare_sequence(seq1);
+    nw::prepare_sequence(seq2);
 
-// Print results
+    // Construct score matrix (reverse the sequence)
+    const size_t NROW = seq1.length(),
+                 NCOL = seq2.length();
+    vector<vector<float>> matrix(NROW + 1,
+                                 vector<float>(NCOL + 1));
+    nw::initialize_score_matrix(matrix, GAP);
+    // Calculate score
+    for (size_t i = 1; i <= NROW; i++)
+    {
+        for (size_t j = 1; j <= NCOL; j++)
+        {
+            matrix[i][j] = nw::max_score(nw::score_left(matrix, i, j, GAP),
+                                         nw::score_top_left(matrix, i, j,
+                                                            seq1, seq2,
+                                                            MATCH, MISMATCH),
+                                         nw::score_top(matrix, i, j, GAP));
+        }
+    }
+    // Trace back and find the alignment
+
+    // Print results
+    nw::print_score_matrix(matrix, seq1, seq2);
+
+    return 0;
+}
--- Needleman_Wunsch/src/nw.cpp
+++ Needleman_Wunsch/src/nw.cpp
@@ -2,3 +2,131 @@
 
 using std::string;
 using std::vector;
+
+namespace nw
+{
+string read_fasta(const string input_file)
+{
+    std::ifstream fin(input_file);
+    std::stringstream buffer;
+    string firstline;
+    if (fin.is_open())
+    {
+        getline(fin, firstline); // skip description line
+        buffer << fin.rdbuf();
+    }
+    else
+    {
+        printf("Cannot open file: %s\n", input_file.c_str());
+        exit(1);
+    }
+    return buffer.str();
+}
+void strip_newline(string &s)
+{
+    for (size_t i = s.find('\n'); i != string::npos; i = s.find('\n'))
+    {
+        s.erase(i);
+    }
+}
+void to_upper_case(string &s)
+{
+    // using ASCII difference between upper and lower cases
+    for (auto &c : s)
+    {
+        if (c >= 'a' && c <= 'z')
+        {
+            c -= 32;
+        }
+    }
+}
+void reverse_string(string &s)
+{
+    const size_t l = s.length();
+    for (size_t i = 0; i < l / 2; i++)
+    {
+        std::swap(s[i], s[l - i - 1]);
+    }
+}
+void prepare_sequence(string &s)
+{
+    strip_newline(s);
+    to_upper_case(s);
+    reverse_string(s);
+}
+void initialize_score_matrix(vector<vector<float>> &m, float gap_score)
+{
+    m[0][0] = 0;
+    for (size_t i = 1; i < m.size(); i++)
+    {
+        m[i][0] = m[i - 1][0] + gap_score;
+    }
+    for (size_t j = 1; j < m[0].size(); j++)
+    {
+        m[0][j] = m[0][j - 1] + gap_score;
+    }
+}
+float score_top_left(vector<vector<float>> &m,
+                     size_t i, size_t j,
+                     string &seq1, string &seq2,
+                     float match_score, float mismatch_score)
+{
+    float score;
+    if (seq1[i - 1] == seq2[j - 1])
+    {
+        score = m[i - 1][j - 1] + match_score;
+    }
+    else
+    {
+        score = m[i - 1][j - 1] + mismatch_score;
+    }
+    return score;
+}
+
+float score_left(std::vector<std::vector<float>> &m,
+                 size_t i, size_t j, float gap_score)
+{
+    return m[i][j - 1] + gap_score;
+}
+
+float score_top(std::vector<std::vector<float>> &m,
+                size_t i, size_t j, float gap_score)
+{
+    return m[i - 1][j] + gap_score;
+}
+float max_score(float a, float b, float c)
+{
+    float max = a;
+    if (b > max)
+    {
+        max = b;
+    }
+    if (c > max)
+    {
+        max = c;
+    }
+    return max;
+}
+void print_score_matrix(vector<vector<float>> &m,
+                        string &seq1, string &seq2)
+{
+    printf(",Seq2");
+    for (auto &c : seq2)
+    {
+        printf(",%c", c);
+    }
+    printf("\nSeq1");
+    for (auto &score : m[0])
+    {
+        printf(",%.1f", score);
+    }
+    for (size_t i = 1; i <= seq1.length(); i++)
+    {
+        printf("\n%c", seq1[i - 1]);
+        for (auto &score : m[i])
+        {
+            printf(",%.1f", score);
+        }
+    }
+}
+} // namespace nw
