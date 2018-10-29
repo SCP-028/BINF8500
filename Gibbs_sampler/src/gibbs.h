c1651dae41583487d71714001648a2362c9fc7df
SCP-028 <zhou.zy.yi@gmail.com>
2018-10-29 11:30:46 -0400

    Gibbs sampler for unsupervised motif finding.
    Currently the code is able to detect motifs given a certain length.

--- Gibbs_sampler/src/gibbs.h
+++ Gibbs_sampler/src/gibbs.h
@@ -9,7 +9,7 @@
 #include <vector>
 using namespace std;
 
-const static float PSEUDOCOUNT = 0.25;
+const static double PSEUDOCOUNT = 0.125;
 
 template <typename T>
 using Matrix = vector<vector<T>>;
@@ -45,13 +45,14 @@ inline Matrix<string> read_fasta(const string &input_file) {
 }  // namespace infiles
 
 namespace pssm {
-inline Matrix<float> init_freq_matrix(const float pc, const float motif_len) {
-    // Creates a motif_len by 4 matrix filled with pseudo-counts
+inline Matrix<double> init_freq_matrix(const double pc,
+                                       const double motif_len) {
+    // Creates a `motif_len` by 4 matrix filled with pseudo-counts
     // as initial values
-    Matrix<float> ans(motif_len, vector<float>(4, pc));
+    Matrix<double> ans(motif_len, vector<double>(4, pc));
     return ans;
 }
-inline void count_char_occurrence(const string &s, vector<float> &bg_freq) {
+inline void count_bg_freq(const string &s, vector<double> &bg_freq) {
     for (auto &c : s) {
         switch (c) {
             case 'A':
@@ -71,19 +72,24 @@ inline void count_char_occurrence(const string &s, vector<float> &bg_freq) {
                 break;
         }
     }
+    double total_score = 0.0;
+    for (auto &score : bg_freq) {
+        total_score += score;
+    }
     for (auto &freq : bg_freq) {
-        freq /= s.size();
+        freq /= total_score;
     }
 }
-inline void count_char_occurrence(const string &s, Matrix<float> &freq_matrix) {
+inline void count_char_occurrence(const string &s, const size_t s_idx,
+                                  const size_t motif_len,
+                                  Matrix<double> &freq_matrix) {
     //     pos      A  C  G  T
     //      0       pc pc pc pc
     //      1       pc pc pc pc
     //     ...      ...........
     // motif_len-1  pc pc pc pc
-    const size_t motif_len = s.size();
     for (size_t i = 0; i < motif_len; i++) {
-        switch (s[i]) {
+        switch (s[i + s_idx]) {
             case 'A':
                 freq_matrix[i][0] += 1;
                 break;
@@ -102,22 +108,23 @@ inline void count_char_occurrence(const string &s, Matrix<float> &freq_matrix) {
         }
     }
 }
-inline void convert_to_pssm(const vector<float> &bg_freq,
-                            Matrix<float> &freq_matrix) {
+inline void convert_to_pssm(const vector<double> &bg_freq,
+                            Matrix<double> &freq_matrix) {
     // not taking log because can't have negative scores
     for (auto &row : freq_matrix) {
-        float total_freq = 0.0;
+        double total_freq = 0.0;
         for (auto &ele : row) {
             total_freq += ele;
         }
         for (int i = 0; i < 4; i++) {
-            row[i] /= total_freq * bg_freq[i];
+            row[i] = log(row[i] / (total_freq * bg_freq[i]));
+            row[i] = (row[i] < 0) ? 0 : row[i];
         }
     }
 }
-inline float calc_score(const Matrix<float> &pssm, const string &s,
-                        const size_t start_pos, const size_t motif_len) {
-    float total_score = 0.0;
+inline double calc_score(const Matrix<double> &pssm, const string &s,
+                         const size_t start_pos, const size_t motif_len) {
+    double total_score = 0.0;
     for (size_t i = 0; i < motif_len; i++) {
         switch (s[i + start_pos]) {
             case 'A':
@@ -139,21 +146,20 @@ inline float calc_score(const Matrix<float> &pssm, const string &s,
     }
     return total_score;
 }
-inline Matrix<float> build_pssm(const Matrix<string> &fasta,
-                                const size_t motif_len, const size_t seq_i,
-                                vector<size_t> &motif_positions) {
-    vector<float> bg_freq(4, 0.0);
-    Matrix<float> score_matrix = pssm::init_freq_matrix(PSEUDOCOUNT, motif_len);
+inline Matrix<double> build_pssm(const Matrix<string> &fasta,
+                                 const size_t motif_len, const size_t seq_i,
+                                 vector<size_t> &motif_positions) {
+    vector<double> bg_freq(4, 0.0);
+    Matrix<double> score_matrix = pssm::init_freq_matrix(0, motif_len);
     const size_t fasta_size = fasta.size();
     for (size_t i = 0; i < fasta_size; i++) {
         // use left-out sequence to calculate background
         // frequency, and use other motifs to build PSSM
         if (i == seq_i) {
-            pssm::count_char_occurrence(fasta[i][1], bg_freq);
+            pssm::count_bg_freq(fasta[i][1], bg_freq);
         } else {
-            string motif_seq =
-                fasta[i][1].substr(motif_positions[i], motif_len);
-            pssm::count_char_occurrence(motif_seq, score_matrix);
+            pssm::count_char_occurrence(fasta[i][1], motif_positions[i],
+                                        motif_len, score_matrix);
         }
     }
     pssm::convert_to_pssm(bg_freq, score_matrix);
@@ -164,46 +170,51 @@ inline Matrix<float> build_pssm(const Matrix<string> &fasta,
 namespace gibbs {
 inline vector<size_t> init_motif_positions(const Matrix<string> &seqs,
                                            const size_t motif_len,
-                                           mt19937 seed) {
+                                           mt19937 &generator) {
     vector<size_t> motif_positions;
     motif_positions.reserve(seqs.size());
     for (auto &seq : seqs) {
-        uniform_int_distribution<size_t> range(0, seq[1].size() - motif_len);
-        motif_positions.emplace_back(range(seed));
+        uniform_int_distribution<size_t> dist(0, seq[1].size() - motif_len);
+        motif_positions.emplace_back(dist(generator));
     }
     return motif_positions;
 }
-vector<float> calc_scores_in_seq(const Matrix<string> &fasta,
-                                 const size_t seq_i, const float motif_len,
-                                 const Matrix<float> &score_matrix) {
+inline vector<double> calc_scores_in_seq(const Matrix<string> &fasta,
+                                         const size_t seq_i,
+                                         const double motif_len,
+                                         const Matrix<double> &score_matrix) {
     const size_t motif_num = fasta[seq_i][1].size() - motif_len;
-    vector<float> motif_scores(motif_num, 0.0);
+    vector<double> motif_scores(motif_num, 0.0);
     for (size_t _pos = 0; _pos < motif_num; _pos++) {
         motif_scores[_pos] =
             pssm::calc_score(score_matrix, fasta[seq_i][1], _pos, motif_len);
     }
     return motif_scores;
 }
-size_t update_position(const Matrix<string> &fasta, const size_t motif_len,
-                       mt19937 seed, vector<size_t> &motif_positions,
-                       vector<float> &fasta_scores) {
+inline size_t update_position(const Matrix<string> &fasta,
+                              const size_t motif_len,
+                              vector<size_t> &motif_positions,
+                              vector<double> &fasta_scores,
+                              mt19937 &generator) {
     size_t num_changes = 0;
     const size_t seq_num = fasta.size();
     for (size_t i = 0; i < seq_num; i++) {
         // Use all words except seq[i] to build the PSSM
-        Matrix<float> score_matrix =
+        Matrix<double> score_matrix =
             pssm::build_pssm(fasta, motif_len, i, motif_positions);
 
         // Use PSSM to assign score to each position in seq 1
-        vector<float> motif_scores =
+        vector<double> motif_scores =
             gibbs::calc_scores_in_seq(fasta, i, motif_len, score_matrix);
+        size_t new_pos = 0;
 
-        // Select new location in seq 1 with probability proportional to the
-        // previous scores using a weighted probability distribution
-        std::discrete_distribution<size_t> generator(motif_scores.begin(),
-                                                     motif_scores.end());
-        size_t current_pos = motif_positions[i], new_pos = generator(seed);
-        if (new_pos != current_pos) {
+        // Select new location in seq 1 with probability proportional
+        // to the previous scores using a weighted probability distribution
+
+        std::discrete_distribution<size_t> dist(motif_scores.begin(),
+                                                motif_scores.end());
+        new_pos = dist(generator);
+        if (new_pos != motif_positions[i]) {
             num_changes++;
             motif_positions[i] = new_pos;
             fasta_scores[i] =
@@ -212,16 +223,79 @@ size_t update_position(const Matrix<string> &fasta, const size_t motif_len,
     }
     return num_changes;
 }
-inline void update_score(const vector<float> &fasta_scores,
-                         const vector<size_t> &motif_positions,
-                         float &max_score, vector<size_t> &final_positions) {
-    float total_score = 0.0;
+inline void update_final_score(const vector<double> &fasta_scores,
+                               const vector<size_t> &motif_positions,
+                               double &max_score,
+                               vector<size_t> &final_positions) {
+    double total_score = 0.0;
     for (auto &ele : fasta_scores) {
         total_score += ele;
     }
     if (total_score > max_score) {
         max_score = total_score;
         final_positions = motif_positions;
+    }
+}
+inline void shift_left(const Matrix<string> &fasta, const size_t motif_len,
+                       const size_t step, vector<size_t> &motif_positions,
+                       vector<double> &fasta_scores) {
+    // Shift left one nt if not already at position 0
+    const size_t seq_num = fasta.size();
+    for (size_t i = 0; i < seq_num; i++) {
+        if (motif_positions[i] >= step) {
+            motif_positions[i] -= step;
+        }
+        Matrix<double> score_matrix =
+            pssm::build_pssm(fasta, motif_len, i, motif_positions);
+        double new_score = pssm::calc_score(score_matrix, fasta[i][1],
+                                            motif_positions[i], motif_len);
+        if (new_score > fasta_scores[i]) {
+            fasta_scores[i] = new_score;
+        } else {
+            motif_positions[i] += step;
+        }
+    }
+}
+inline void shift_right(const Matrix<string> &fasta, const size_t motif_len,
+                        const size_t step, vector<size_t> &motif_positions,
+                        vector<double> &fasta_scores) {
+    const size_t seq_num = fasta.size();
+    for (size_t i = 0; i < seq_num; i++) {
+        if (motif_positions[i] + motif_len + step <= fasta[i][1].size()) {
+            motif_positions[i] += step;
+        }
+        Matrix<double> score_matrix =
+            pssm::build_pssm(fasta, motif_len, i, motif_positions);
+        double new_score = pssm::calc_score(score_matrix, fasta[i][1],
+                                            motif_positions[i], motif_len);
+        if (new_score > fasta_scores[i]) {
+            fasta_scores[i] = new_score;
+        } else {
+            motif_positions[i] -= step;
+        }
+    }
+}
+inline void final_scan(const Matrix<string> &fasta, const size_t motif_len,
+                       vector<size_t> &motif_positions,
+                       vector<double> &fasta_scores) {
+    const size_t seq_num = fasta.size();
+    for (size_t i = 0; i < seq_num; i++) {
+        // Use all words except seq[i] to build the PSSM
+        Matrix<double> score_matrix =
+            pssm::build_pssm(fasta, motif_len, i, motif_positions);
+
+        // Use PSSM to assign score to each position in seq 1
+        vector<double> motif_scores =
+            gibbs::calc_scores_in_seq(fasta, i, motif_len, score_matrix);
+        double high_score = 0.0;
+        for (size_t pos = 0; pos < motif_scores.size(); pos++) {
+            if (motif_scores[pos] > high_score) {
+                high_score = motif_scores[pos];
+                motif_positions[i] = pos;
+            }
+        }
+        fasta_scores[i] = pssm::calc_score(score_matrix, fasta[i][1],
+                                           motif_positions[i], motif_len);
     }
 }
 }  // namespace gibbs
--- Gibbs_sampler/src/main.cpp
+++ Gibbs_sampler/src/main.cpp
@@ -5,7 +5,9 @@
 #include "gibbs.h"
 using namespace std;
 
-const static size_t MAX_ITER = 10000;
+const static size_t INIT_SEED = 20, MAX_ITER = 1000;
+// chance of shifting left & right in each iteration
+const static double PROB_SHIFT = 0.20, PROB_CHANGE_LEN = 0.01;
 
 int main(int argc, char **argv) {
     if (argc != 3) {
@@ -16,32 +18,71 @@ int main(int argc, char **argv) {
             argv[0], argc - 1, argv[0], argv[0]);
         return 1;
     }
-    // Random number generator
     random_device rn;
-    mt19937 seed{rn()};
-
     // Read fasta file into matrix of strings, where:
     //     the first column is the name, and
     //     the second column is the sequence
     const Matrix<string> fasta = infiles::read_fasta(argv[1]);
 
-    // Initialization - randomly select a motif in each sequence
+    // Global parameters
     size_t motif_len = stoul(argv[2]);
-    const size_t fasta_size = fasta.size();
-    vector<size_t> motif_positions =
-                       gibbs::init_motif_positions(fasta, motif_len, seed),
-                   final_positions;
+    uniform_real_distribution<double> shift_left_right(0, 1);
 
-    vector<float> fasta_scores(fasta_size, 0.0);
-    float max_score = 0.0;
-    size_t num_changes = 1;
-    size_t iter_num = 0;
-    while (num_changes != 0 && iter_num <= MAX_ITER) {
-        iter_num++;
-        num_changes = gibbs::update_position(fasta, motif_len, seed,
-                                             motif_positions, fasta_scores);
-        gibbs::update_score(fasta_scores, motif_positions, max_score,
-                            final_positions);
+    const size_t fasta_size = fasta.size();
+    vector<float> max_scores(INIT_SEED);
+    Matrix<size_t> final_positions(INIT_SEED);
+#pragma omp parallel for
+    for (size_t init_stat = 0; init_stat < INIT_SEED; init_stat++) {
+        double max_score = 0.0;
+        vector<double> fasta_scores(fasta_size, 0.0);
+        vector<size_t> final_position;
+
+        // Initialization - randomly select a motif in each sequence
+        mt19937 generator{rn()};
+        vector<size_t> motif_positions =
+            gibbs::init_motif_positions(fasta, motif_len, generator);
+
+        size_t iter_num = 0, num_changes = 1;
+
+        while (num_changes != 0 && iter_num <= MAX_ITER) {
+            iter_num++;
+            // Update motif position in each sequence to the position with the
+            // highest score
+            num_changes = gibbs::update_position(
+                fasta, motif_len, motif_positions, fasta_scores, generator);
+            gibbs::update_final_score(fasta_scores, motif_positions, max_score,
+                                      final_position);
+
+            // Shift left or right with a probability
+            if (shift_left_right(generator) <= PROB_SHIFT) {
+                num_changes++;
+                gibbs::shift_left(fasta, motif_len, 1, motif_positions,
+                                  fasta_scores);
+                gibbs::update_final_score(fasta_scores, motif_positions,
+                                          max_score, final_position);
+
+                gibbs::shift_right(fasta, motif_len, 2, motif_positions,
+                                   fasta_scores);
+                gibbs::update_final_score(fasta_scores, motif_positions,
+                                          max_score, final_position);
+            }
+        }
+        for (size_t i = 0; i < fasta_size; i++) {
+            gibbs::final_scan(fasta, motif_len, motif_positions, fasta_scores);
+        }
+        gibbs::update_final_score(fasta_scores, motif_positions, max_score,
+                                  final_position);
+        max_scores[init_stat] = max_score;
+        final_positions[init_stat] = final_position;
+    }
+
+    double max_score = 0.0;
+    vector<size_t> final_position;
+    for (size_t i = 0; i < INIT_SEED; i++) {
+        if (max_scores[i] > max_score) {
+            max_score = max_scores[i];
+            final_position = final_positions[i];
+        }
     }
 
     // Print results
@@ -55,10 +96,11 @@ int main(int argc, char **argv) {
 
     printf("Motif sequences and locations:\n\n");
     for (size_t i = 0; i < fasta_size; i++) {
-        const size_t motif_pos = final_positions[i];
+        const size_t motif_pos = final_position[i];
         const string motif_seq = fasta[i][1].substr(motif_pos, motif_len);
         printf("%s\t%zu-%zu\t%s\n", motif_seq.c_str(), motif_pos + 1,
                motif_pos + motif_len, fasta[i][0].c_str());
     }
+    printf("\n\n");
     return 0;
 }
